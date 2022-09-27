4d46b598be12f4369655fe3375ccdf497fffab4d
20Youssef03 <91949220+20Youssef03@users.noreply.github.com>
2022-09-27 16:20:26 +0200

    1.2

---
+++ Settings Window.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Arcade_Game_2022
+{
+    /// <summary>
+    /// Interaction logic for Settings_Window.xaml
+    /// </summary>
+    public partial class Settings_Window : Window
+    {
+        public Settings_Window()
+        {
+            InitializeComponent();
+        }
+    }
+}
