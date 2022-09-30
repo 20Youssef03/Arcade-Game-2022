28d026c8ede125494cc210809c01b5d82e550ee5
Simone <simone.kooistra1@student.nhlstenden.com>
2022-09-30 12:00:05 +0200

    7.5

--- obj/Release/netcoreapp3.1/App.g.cs
+++ obj/Release/netcoreapp3.1/App.g.cs
@@ -45,7 +45,7 @@ namespace Arcade_Game_2022 {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.8.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace Arcade_Game_2022 {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.8.0")]
         public static void Main() {
             Arcade_Game_2022.App app = new Arcade_Game_2022.App();
             app.InitializeComponent();
